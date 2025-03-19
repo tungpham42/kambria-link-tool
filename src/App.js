7dfea54f42e4bcab8b6d50ab8b98116f2b8b6717
Tung Pham <tung.42@gmail.com>
2025-03-19 12:58:38 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import UrlConverter from "./components/UrlConverter";
+import RomanizeTool from "./components/RomanizeTool";
 
 const App = () => {
   return (
     <>
       <UrlConverter />
+      <RomanizeTool />
       <Footer />
     </>
   );
