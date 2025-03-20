237939e94871eda7005afb058e4241d5fe968157
Tung Pham <tung.42@gmail.com>
2025-03-20 14:38:17 +0700

    light

--- src/components/RomanizeTool.js
+++ src/components/RomanizeTool.js
@@ -63,7 +63,7 @@ const RomanizeTool = () => {
   return (
     <Container className="my-5 col-lg-8 col-md-10 col-sm-10 col-12">
       <Card className="shadow-lg rounded">
-        <Card.Header className="d-flex align-items-center justify-content-center bg-dark text-light">
+        <Card.Header className="d-flex align-items-center justify-content-center">
           <h2 className="text-center m-0">Slug Tool</h2>
           <Button
             variant="link"
