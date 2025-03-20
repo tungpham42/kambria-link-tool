7d2c3b21f7f6c222a06ca65a0b30faf05c7c88d5
Tung Pham <tung.42@gmail.com>
2025-03-20 11:48:53 +0700

    Update RomanizeTool.js

--- src/components/RomanizeTool.js
+++ src/components/RomanizeTool.js
@@ -146,6 +146,8 @@ const RomanizeTool = () => {
                 <li>"This & That" → "this-that" (9 characters)</li>
                 <li>"Café Olé" → "cafe-ole" (8 characters)</li>
                 <li>"Multiple Spaces" → "multiple-spaces" (15 characters)</li>
+                <li>"こんにちは" → "konnitiha"</li>
+                <li>"안녕하세요" → "annyeonghaseyo"</li>
               </ul>
             </Card.Body>
           </Card>
